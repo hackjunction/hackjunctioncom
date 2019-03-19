e73e37e872978f86020a302954dbd85bb8e3a694
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-03-19 15:16:10 +0200

    minor fixes to HeaderVideo

--- frontend/src/components/HeaderImage/HeaderVideo.js
+++ frontend/src/components/HeaderImage/HeaderVideo.js
@@ -12,7 +12,12 @@ const HeaderVideo = ({ videoSource, posterImage, alt, navTitle }) => {
 		<div className={`HeaderImage ${imageLoaded ? 'HeaderImage-loaded' : ''}`}>
 			<Image image={posterImage} width={1920} height={900} alt={alt} className="HeaderImage--img" onLoad={() => setImageLoaded(true)} />
 			<div className={`HeaderImage--vid ${videoLoaded ? 'HeaderImage--vid-visible' : ''}`}>
-				<video autoPlay onPlay={() => setVideoLoaded(true)} data-dashjs-player loop src={videoSource}></video>
+				<video
+					autoPlay
+					onPlay={() => setVideoLoaded(true)}
+					data-dashjs-player
+					src={videoSource}>
+				</video>
 			</div>
 			<img className="HeaderImage--wave-overlay" src={require('../../assets/misc/wave-ltr.png')} alt="Wave overlay" />
 			{navTitle ? <h1 className="HeaderImage--nav-title">{navTitle}</h1> : null}
