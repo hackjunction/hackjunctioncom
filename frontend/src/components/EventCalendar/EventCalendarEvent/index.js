cd7789cf1ee724c0e4d88c524a7cbea2c61aecda
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-03-25 18:27:26 +0200

    add schema.org definitions for events

--- frontend/src/components/EventCalendar/EventCalendarEvent/index.js
+++ frontend/src/components/EventCalendar/EventCalendarEvent/index.js
@@ -27,21 +27,26 @@ const EventCalendarEvent = ({ event, getDefaultImage }) => {
 	}
 
 	return (
-		<div className="EventCalendarEvent" style={{ '--theme-color': getEventColor() }}>
-			<h4 className="EventCalendarEvent--name">{event.name}</h4>
+		<div
+			className="EventCalendarEvent"
+			style={{ '--theme-color': getEventColor() }}
+			itemscope
+			itemtype="http://schema.org/Event"
+		>
+			<h4 className="EventCalendarEvent--name" itemprop="name">{event.name}</h4>
 			<div className="EventCalendarEvent--details">
 				<div className="EventCalendarEvent--detail">
 					<i className="EventCalendarEvent--detail__icon"></i>
-					<span className="EventCalendarEvent--detail__text">{moment(event.begins).format('DD.MM.YYYY')}</span>
+					<span className="EventCalendarEvent--detail__text" itemprop="startDate">{moment(event.begins).format('DD.MM.YYYY')}</span>
 				</div>
 				<div className="EventCalendarEvent--detail">
 					<div className="EventCalendarEvent--detail__icon">
 						<i className="EventCalendarEvent--detail__icon-inner"></i>
 					</div>
-					<span className="EventCalendarEvent--detail__text">{event.locationdescription}</span>
+					<span className="EventCalendarEvent--detail__text" itemprop="location">{event.locationdescription}</span>
 				</div>
 			</div>
-			<a className="EventCalendarEvent--pagelink" href={event.linkToEventSite}>
+			<a className="EventCalendarEvent--pagelink" itemprop="url" href={event.linkToEventSite}>
 				<span className="EventCalendarEvent--pagelink__text">Event page</span>
 			</a>
 		</div>
