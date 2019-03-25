caa7fad5fa5c8329b364a1b11690d69da1cd302f
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-03-25 11:12:24 +0200

    move ga pageview tracking to PageHOC

--- frontend/src/pages/PageHOC/index.js
+++ frontend/src/pages/PageHOC/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Helmet } from 'react-helmet'
+import ReactGA from 'react-ga';
+import config from '../../services/config'
+
+if (config.GOOGLE_ANALYTICS_ID) {
+	ReactGA.initialize(config.GOOGLE_ANALYTICS_ID);
+} else {
+	console.log('DEBUG: config variable GOOGLE_ANALYTICS_ID undefined, not initializing GA tracking')
+}
+
 
 const DEFAULT_OG_IMAGE = require('../../assets/images/default_image.jpg')
 
 const PageHOC = ({ className, children, pageTitle, metaDesc, ogImageUrl = DEFAULT_OG_IMAGE }) => {
+
+	/* Track pageView in Google Analytics on mount */
+	useEffect(() => {
+		ReactGA.pageview(window.location.pathname + window.location.search);
+	}, [])
 
 	const canonicalUrl = 'https://' + window.location.hostname + window.location.pathname
 
--- frontend/src/redux/nav/reducer.js
+++ frontend/src/redux/nav/reducer.js
@@ -1,13 +1,5 @@
 import * as ActionTypes from './actionTypes'
 import { LOCATION_CHANGE } from 'connected-react-router'
-import ReactGA from 'react-ga';
-import config from '../../services/config'
-
-if (config.GOOGLE_ANALYTICS_ID) {
-	ReactGA.initialize(config.GOOGLE_ANALYTICS_ID);
-} else {
-	console.log('DEBUG: config variable GOOGLE_ANALYTICS_ID undefined, not initializing GA tracking')
-}
 
 const intialState = {
 	sidebarOpen: false,
@@ -23,7 +15,6 @@ export default function reducer(state = intialState, action) {
 		}
 
 		case LOCATION_CHANGE: {
-			ReactGA.pageview(window.location.pathname + window.location.search);
 			return {
 				...state,
 				sidebarOpen: false,
