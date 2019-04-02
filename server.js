3e37ffd98dc0f9666de44714e1d583ec533f4be0
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-04-02 16:26:40 +0300

    Add ssl enforce

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ var express = require('express'),
 	path = require('path'),
 	port = process.env.PORT || 3000;
 
+var enforce = require('express-sslify');
+
+
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 // Handle React routing, return all requests to React app
 app.get('*', function (req, res) {
 	res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.listen(port);
