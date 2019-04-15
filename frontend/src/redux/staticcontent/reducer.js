9236270819a0963dd2d9e4508c345ae37902a998
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-04-15 14:19:39 +0300

    minor fixes

--- frontend/src/redux/staticcontent/reducer.js
+++ frontend/src/redux/staticcontent/reducer.js
@@ -14,17 +14,25 @@ export default function reducer(state = initialState, action) {
 
 	switch (action.type) {
 		case ActionTypes.UPDATE_STATIC_CONTENT: {
-			const data = {}
-			const keys = map(action.payload, ({ key, content }) => {
-				data[key.trim()] = content;
-				return key.trim();
-			});
-
 			return handle(state, action, {
 				start: prevState => ({ ...prevState, loading: true, error: false }),
 				finish: prevState => ({ ...prevState, loading: false }),
 				failure: prevState => ({ ...prevState, error: true }),
-				success: prevState => ({ ...prevState, data, keys, lastUpdate: Date.now() }),
+				success: prevState => {
+
+					const data = {}
+					const keys = map(action.payload, ({ key, content }) => {
+						data[key.trim()] = content;
+						return key.trim();
+					});
+
+					return {
+						...prevState,
+						data,
+						keys,
+						lastUpdate: Date.now()
+					}
+				},
 			})
 		}
 		default: return state;
--- frontend/src/redux/staticmedia/reducer.js
+++ frontend/src/redux/staticmedia/reducer.js
@@ -14,16 +14,26 @@ export default function reducer(state = initialState, action) {
 
 	switch (action.type) {
 		case ActionTypes.UPDATE_STATIC_MEDIA: {
-			const data = {}
-			const keys = map(action.payload, ({ key, media }) => {
-				data[key.trim()] = media;
-				return key.trim();
-			})
+
 			return handle(state, action, {
 				start: prevState => ({ ...prevState, loading: true, error: false }),
 				finish: prevState => ({ ...prevState, loading: false }),
 				failure: prevState => ({ ...prevState, error: true }),
-				success: prevState => ({ ...prevState, data, keys, lastUpdate: Date.now() }),
+				success: prevState => {
+
+					const data = {}
+					const keys = map(action.payload, ({ key, media }) => {
+						data[key.trim()] = media;
+						return key.trim();
+					})
+
+					return {
+						...prevState,
+						data,
+						keys,
+						lastUpdate: Date.now()
+					}
+				},
 			})
 		}
 		default: return state;
