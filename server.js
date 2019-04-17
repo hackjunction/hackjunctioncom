93ead47c840fd9ba303f2108337ee21ff85aebbd
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-04-17 15:09:15 +0300

    add prerendercloud

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ var express = require('express'),
 	path = require('path'),
 	port = process.env.PORT || 3000;
 
+// Make sure crawlers understand the site
+const prerendercloud = require('prerendercloud');
+prerendercloud.set('botsOnly', true);
+app.use(prerendercloud);
+
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-// Make sure crawlers understand the site
-app.use(require('prerender-node'));
 
 // Handle React routing, return all requests to React app
 app.get('*', function (req, res) {
