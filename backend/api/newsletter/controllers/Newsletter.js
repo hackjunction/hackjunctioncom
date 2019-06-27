9969a06d875e6640f627f939efd6ebe18bdefbbf
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-06-27 13:57:43 +0300

    Begin migration to strapi beta

--- backend/api/newsletter/controllers/Newsletter.js
+++ backend/api/newsletter/controllers/Newsletter.js
@@ -6,11 +6,12 @@
  * @description: A set of functions called "actions" for managing `Newsletter`.
  */
 
-const axios = require('axios')
-const validate = require('./validate')
+const axios = require('axios');
+const validate = require('./validate');
 
-const ADD_TO_LIST_ENDPOINT = (list_id, recipient_id) => `https://api.sendgrid.com/v3/contactdb/lists/${list_id}/recipients/${recipient_id}`
-const ADD_RECIPIENT_ENDPOINT = 'https://api.sendgrid.com/v3/contactdb/recipients'
+const ADD_TO_LIST_ENDPOINT = (list_id, recipient_id) =>
+  `https://api.sendgrid.com/v3/contactdb/lists/${list_id}/recipients/${recipient_id}`;
+const ADD_RECIPIENT_ENDPOINT = 'https://api.sendgrid.com/v3/contactdb/recipients';
 
 module.exports = {
   create: async (ctx, res) => {
@@ -19,51 +20,54 @@ module.exports = {
     if (!validate(country, email)) {
       return ctx.send({
         message: 'Validation error'
-      })
+      });
     }
 
-    return axios.post(
-      ADD_RECIPIENT_ENDPOINT,
-      [
-        {
-          email: ctx.request.body.email,
-          country: ctx.request.body.country
-        },
-      ],
-      {
-        timeout: 1000,
-        headers: {
-          'Authorization': 'Bearer ' + strapi.config.currentEnvironment.sendgridApiKey,
-          'Content-Type': 'application/json',
-        }
-      }
-    ).then(res => {
-      if (Array.isArray(res.data.persisted_recipients) && res.data.persisted_recipients.length > 0) {
-        const recipientID = res.data.persisted_recipients[0];
-        const listID = strapi.config.currentEnvironment.sendgridEmailList; //7150117 Junction Newsletter recipients 2019
-        return axios.post(
-          ADD_TO_LIST_ENDPOINT(listID, recipientID),
-          {},
+    return axios
+      .post(
+        ADD_RECIPIENT_ENDPOINT,
+        [
           {
-            timeout: 1000,
-            headers: {
-              'Authorization': 'Bearer ' + strapi.config.currentEnvironment.sendgridApiKey,
-              'Content-Type': 'application/json',
-            }
+            email: ctx.request.body.email,
+            country: ctx.request.body.country
           }
-        );
-      }
-      return Promise.resolve();
-    }).then(res => {
-      ctx.send({
-        message: 'success'
+        ],
+        {
+          timeout: 1000,
+          headers: {
+            Authorization: 'Bearer ' + strapi.config.currentEnvironment.sendgridApiKey,
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      .then(res => {
+        if (Array.isArray(res.data.persisted_recipients) && res.data.persisted_recipients.length > 0) {
+          const recipientID = res.data.persisted_recipients[0];
+          const listID = strapi.config.currentEnvironment.sendgridEmailList; //7150117 Junction Newsletter recipients 2019
+          return axios.post(
+            ADD_TO_LIST_ENDPOINT(listID, recipientID),
+            {},
+            {
+              timeout: 1000,
+              headers: {
+                Authorization: 'Bearer ' + strapi.config.currentEnvironment.sendgridApiKey,
+                'Content-Type': 'application/json'
+              }
+            }
+          );
+        }
+        return Promise.resolve();
       })
-    })
-      .catch(err => {
-        console.log('/api/newsletters: SG ERR ADDING CONTACT', err.response.data)
+      .then(res => {
         ctx.send({
-          message: 'error',
-        })
+          message: 'success'
+        });
       })
-  },
+      .catch(err => {
+        console.log('/api/newsletters: SG ERR ADDING CONTACT', err.response.data);
+        ctx.send({
+          message: 'error'
+        });
+      });
+  }
 };
