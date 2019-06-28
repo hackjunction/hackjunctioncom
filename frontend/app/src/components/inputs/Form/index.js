80d618aa806b10adcaa61987607277b5e5e835d4
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-06-28 13:17:14 +0300

    Fix ugly add form submit overlay

--- frontend/app/src/components/inputs/Form/index.js
+++ frontend/app/src/components/inputs/Form/index.js
@@ -1,73 +1,70 @@
-import React from 'react'
-import './style.scss'
+import React from 'react';
+import './style.scss';
 import { forOwn, isEmpty } from 'lodash-es';
 
-import LogoSpinner from '../../LogoSpinner'
+import LogoSpinner from '../../LogoSpinner';
 
 const Form = ({
-	children,
-	onSuccess = () => { },
-	onError = () => { },
-	fields,
-	msgDuration = 3000,
-	loading = false,
-	success = false,
-	successTitle = '',
-	successMessage = '',
-	error = false,
-	errorTitle = '',
-	errorMessage = ''
+    children,
+    onSuccess = () => {},
+    onError = () => {},
+    fields,
+    msgDuration = 3000,
+    loading = false,
+    success = false,
+    successTitle = '',
+    successMessage = '',
+    error = false,
+    errorTitle = '',
+    errorMessage = ''
 }) => {
+    function handleSubmit(e) {
+        e.preventDefault();
 
-	function handleSubmit(e) {
-		e.preventDefault()
+        let errors = {};
+        let formData = {};
 
-		let errors = {}
-		let formData = {}
+        forOwn(fields, (field, name) => {
+            const errorMsg = field.validate(field.value);
 
-		forOwn(fields, (field, name) => {
-			const errorMsg = field.validate(field.value)
+            if (errorMsg) {
+                field.setError(errorMsg);
+                errors[name] = errorMsg;
+            } else {
+                field.setError(null);
+                formData[name] = field.value;
+            }
+        });
 
-			if (errorMsg) {
-				field.setError(errorMsg)
-				errors[name] = errorMsg
-			} else {
-				field.setError(null)
-				formData[name] = field.value
-			}
-		})
+        if (!isEmpty(errors)) {
+            onError(errors);
+        } else {
+            onSuccess(formData);
+        }
+    }
 
-		if (!isEmpty(errors)) {
-			onError(errors)
-		} else {
-			onSuccess(formData)
-		}
-	}
+    let className = 'Form';
 
-	let className = 'Form'
+    if (loading) className += ' Form-loading';
+    if (error) className += ' Form-error';
+    if (success) className += ' Form-success';
 
-	if (loading) className += ' Form-loading'
-	if (error) className += ' Form-error'
-	if (success) className += ' Form-success'
+    return (
+        <form className={className} onSubmit={handleSubmit}>
+            <div className="Form--loading-overlay">
+                <LogoSpinner />
+            </div>
+            <div className="Form--success">
+                <span className="Form--success__title">{successTitle}</span>
+                <span className="Form--success__message">{successMessage}</span>
+            </div>
+            <div className="Form--Inner">{children}</div>
+            <div className="Form--error-banner">
+                <span className="Form--error-banner__title">{errorTitle}</span>
+                <span className="Form--error-banner__message">{errorMessage}</span>
+            </div>
+        </form>
+    );
+};
 
-	return (
-		<form className={className} onSubmit={handleSubmit}>
-			<div className="Form--loading-overlay">
-				<LogoSpinner />
-			</div>
-			<div className="Form--success">
-				<span className="Form--success__title">{successTitle}</span>
-				<span className="Form--success__message">{successMessage}</span>
-			</div>
-			<div className="Form--Inner">
-				{children}
-			</div>
-			<div className="Form--error-banner">
-				<span className="Form--error-banner__title">{errorTitle}</span>
-				<span className="Form--error-banner__message">{errorMessage}</span>
-			</div>
-		</form>
-	)
-}
-
-export default Form
+export default Form;
