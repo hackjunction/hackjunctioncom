e8e44eb355f25cbfa8ef4fc0474cb39657b2de13
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2019-10-28 14:11:13 +0200

    add id to newsletter form

--- frontend/app/src/components/NewsLetterForm/index.js
+++ frontend/app/src/components/NewsLetterForm/index.js
@@ -1,104 +1,101 @@
-import React, { useState } from 'react'
-import './style.scss'
+import React, { useState } from 'react';
+import './style.scss';
 
-import Form from '../inputs/Form'
-import TextInput from '../inputs/TextInput'
-import FormRow from '../inputs/FormRow'
-import DropDown from '../inputs/DropDown'
-import SubmitButton from '../inputs/SubmitButton'
-import BlockSection from '../BlockSection'
+import Form from '../inputs/Form';
+import TextInput from '../inputs/TextInput';
+import FormRow from '../inputs/FormRow';
+import DropDown from '../inputs/DropDown';
+import SubmitButton from '../inputs/SubmitButton';
+import BlockSection from '../BlockSection';
 
-import { useFormField } from '../../hooks/formhooks'
-import { isEmail } from '../../utils/regex'
-import { isCountry } from '../../utils/misc'
-import { minDelay } from '../../utils/misc'
+import { useFormField } from '../../hooks/formhooks';
+import { isEmail } from '../../utils/regex';
+import { isCountry } from '../../utils/misc';
+import { minDelay } from '../../utils/misc';
 
-import NewsLetterService from '../../services/newsletter'
-import countries from '../../data/countries.json'
+import NewsLetterService from '../../services/newsletter';
+import countries from '../../data/countries.json';
 
 import KEYS from '../../redux/staticcontent/keys';
 
 const NewsLetterForm = () => {
+    const fields = {
+        email: {
+            ...useFormField('', value => {
+                if (!isEmail(value)) {
+                    return 'This is not a valid email';
+                }
 
-	const fields = {
-		email: {
-			...useFormField('', (value) => {
-				if (!isEmail(value)) {
-					return 'This is not a valid email'
-				}
+                return null;
+            })
+        },
+        country: {
+            ...useFormField('', value => {
+                if (!isCountry(value)) {
+                    return 'Please choose a country';
+                }
 
-				return null
-			}),
-		},
-		country: {
-			...useFormField('', value => {
-				if (!isCountry(value)) {
-					return 'Please choose a country'
-				}
+                return null;
+            })
+        }
+    };
 
-				return null;
-			})
-		}
-	}
+    const [formLoading, setFormLoading] = useState(false);
+    const [formError, setFormError] = useState(false);
+    const [formSuccess, setFormSuccess] = useState(false);
 
-	const [formLoading, setFormLoading] = useState(false)
-	const [formError, setFormError] = useState(false)
-	const [formSuccess, setFormSuccess] = useState(false)
+    async function handleFormSuccess(data) {
+        setFormLoading(true);
 
-	async function handleFormSuccess(data) {
-		setFormLoading(true)
+        minDelay(NewsLetterService.create(data), 1000)
+            .then(() => {
+                setFormSuccess(true);
+                setFormLoading(false);
+            })
+            .catch(e => {
+                console.log('Form error', e);
+                setFormError(true);
+                setFormLoading(false);
+            });
+    }
 
-		minDelay(NewsLetterService.create(data), 1000).then(() => {
-			setFormSuccess(true);
-			setFormLoading(false);
-		}).catch(e => {
-			console.log('Form error', e)
-			setFormError(true);
-			setFormLoading(false);
-		})
-	}
+    function handleFormError(errors) {
+        console.log('ERROR', errors);
+    }
 
-	function handleFormError(errors) {
-		console.log('ERROR', errors)
-	}
+    const options = countries.map(c => ({ value: c, label: c }));
 
-	const options = countries.map(c => ({ value: c, label: c }))
+    return (
+        <BlockSection titleKey={KEYS.newsletterFormTitle} subtitleKey={KEYS.newsletterFormSubtitle}>
+            <div id="subscribe" />
+            <Form
+                fields={fields}
+                onError={handleFormError}
+                onSuccess={handleFormSuccess}
+                loading={formLoading}
+                error={formError}
+                success={formSuccess}
+                errorTitle={'Oops, something went wrong'}
+                errorMessage={'Are you connected to the internet? Please try again.'}
+                successTitle={'Thanks for subscribing!'}
+                successMessage={''}
+            >
+                <FormRow>
+                    <TextInput name="email" placeholder="Email" label="Email" {...fields.email} />
+                    <DropDown
+                        name="country"
+                        placeholder="Choose country"
+                        label="Country"
+                        options={options}
+                        {...fields.country}
+                    />
+                </FormRow>
+                <FormRow>
+                    <SubmitButton text="Subscribe" />
+                </FormRow>
+            </Form>
+        </BlockSection>
+    );
+};
 
-	return (
-		<BlockSection titleKey={KEYS.newsletterFormTitle} subtitleKey={KEYS.newsletterFormSubtitle}>
-			<Form
-				fields={fields}
-				onError={handleFormError}
-				onSuccess={handleFormSuccess}
-				loading={formLoading}
-				error={formError}
-				success={formSuccess}
-				errorTitle={'Oops, something went wrong'}
-				errorMessage={'Are you connected to the internet? Please try again.'}
-				successTitle={'Thanks for subscribing!'}
-				successMessage={''}
-			>
-				<FormRow>
-					<TextInput
-						name="email"
-						placeholder="Email"
-						label="Email"
-						{...fields.email}
-					/>
-					<DropDown
-						name="country"
-						placeholder="Choose country"
-						label="Country"
-						options={options}
-						{...fields.country}
-					/>
-				</FormRow>
-				<FormRow>
-					<SubmitButton text="Subscribe" />
-				</FormRow>
-			</Form>
-		</BlockSection>
-	)
-}
-
-export default NewsLetterForm
+export default NewsLetterForm;
