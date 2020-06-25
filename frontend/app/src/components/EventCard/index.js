caf28b207b6b91c75cad397e2145245c298f798c
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-06-25 05:32:09 +0300

    fix planet

--- frontend/app/src/components/EventCard/index.js
+++ frontend/app/src/components/EventCard/index.js
@@ -1,33 +1,76 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
-  root: {
+    root: {
         minWidth: 275,
         maxWidth: 275,
-  },
-  title: {
-    fontSize: 14,
-  },
+        margin: "20%",
+        background: "#232323",
+        border: "4px solid #FBFBFB",
+        boxSizing: "border-box",
+        borderRadius: "5px",
+    },
+    title: {
+        fontFamily: "Montserrat",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "25px",
+        lineHeight: "30px",
+        display: "flex",
+        alignItems: "center",
+        color: "#73F9EC"
+    },
+    date: {
+        fontFamily: "Montserrat",
+        fontStyle: "normal",
+        fontWeight: "normal",
+        fontSize: "14px",
+        lineHeight: "17px",
+        display: "flex",
+        alignItems: "center",
+        color: "#FBFBFB",        
+    },
+    body: {
+        fontFamily: "Montserrat",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: "18px",
+        lineHeight: "22px",
+        display: "flex",
+        alignItems: "center",
+        color: "#FBFBFB",        
+    },
 });
 
-export default function SimpleCard() {
-  const classes = useStyles();
-  const bull = <span className={classes.bullet}>•</span>;
+const EventCard = (eventData) => {
+    const classes = useStyles();
+    console.log("event data is", eventData, eventData.title)
+    return (
+        eventData? <Card className={classes.root}>
+            <CardContent>
+                <Typography
+                    className={classes.title}
+                    color="textSecondary"
+                    gutterBottom
+                >
+                    {eventData.title}
+                </Typography>
+                <Typography
+                    className={classes.date} variant="body2" component="p">
+                    {eventData.date}
+                </Typography>
 
-  return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Title
-        </Typography>
-        <Typography variant="body2" component="p">
-          Description
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+                <Typography
+                    className={classes.body} variant="body2" component="p">
+                    {eventData.desc}
+                </Typography>
+            </CardContent>
+        </Card>: null
+    );
 }
+
+export default EventCard;
--- frontend/app/src/components/Planet/index.js
+++ frontend/app/src/components/Planet/index.js
@@ -1,51 +1,62 @@
-import React from 'react';
-import * as THREE from 'three/build/three.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import React, { useRef, useEffect, useState }  from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import * as THREE from "three/build/three.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-import EventCard from '../EventCard'
+import EventCard from "../EventCard";
+
+const useStyles = makeStyles({
+    vis: {
+        display: "flex",
+        flexDirection: "row",
+        position: "static",
+        padding: 0,
+    },
+});
+
 const Planet = () => {
-    const { useRef, useEffect, useState } = React;
+    const classes = useStyles();
     const mount = useRef(null);
 
+    const [eventData, setEventData] = useState(null)
     useEffect(() => {
         const RADIUS = 10;
         const SIZE = RADIUS * 0.04;
         const raycaster = new THREE.Raycaster();
         const mouse = new THREE.Vector2();
-        const absoluteMouse = new THREE.Vector2();
         const events = {
             1: {
                 lat: 60.1699,
                 lon: -24.9384,
                 color: 0x00ff00,
-                info: "Junction X Oulu",
+                data: { title: "Junction X Oulu", date: "soon", desc: "huutis ja myötis" },
                 __id: 1,
             },
             2: {
                 lat: 35.9334,
                 lon: -30.8597,
                 color: 0xff0000,
-                info: "Junction X Ankara",
+                data: { title: "Junction X Ankara", date: "soonish", desc: "Hyytis ja myötis" },
                 __id: 2,
             },
             3: {
                 lat: 35.6762,
                 lon: -139.6503,
                 color: 0x0000ff,
-                info: "Junction X Tokyo",
+                data: { title: "Junction X Tokyo", date: "past", desc: "Junction X TokyoJunction X TokyoJunction X TokyoJunction X TokyoJunction X Tokyo" },
                 __id: 3,
             },
             4: {
                 lat: 40.7128,
                 lon: 74.006,
                 color: 0xaa00aa,
-                info: "Junction X NY",
+                data: { title: "Junction X NY", date: "now", desc: "Junction x asteriski and this is qute a bit longer thext that the other just to test that it looks okay on the componsnent. Does it?" },
                 __id: 4,
             },
         };
-        console.log("m", mount)
-        let width = 400//mount.current.clientWidth;
-        let height = 400//mount.current.clientHeight;
+        console.log("m", mount);
+        let width = mount.current.clientWidth / 3;
+        let height = width; // mount.current.clientHeight;
         let frameId;
 
         const scene = new THREE.Scene();
@@ -53,47 +64,47 @@ const Planet = () => {
 
         const camera = new THREE.PerspectiveCamera(
             45,
-            window.innerWidth / window.innerHeight,
+            width / height,
             0.2,
             RADIUS * 10,
         );
-        camera.position.z = 25;
-    
+        camera.position.z = 40;
+
         const renderer = new THREE.WebGLRenderer({ antialias: true });
 
         const controls = new OrbitControls(camera, renderer.domElement);
         controls.enableZoom = false;
-    
+
         renderer.setSize(width, height);
 
         const handleResize = () => {
-            console.log("resixiing")
-            width = mount.current.clientWidth;
-            height = mount.current.clientHeight;
+            console.log("resixiing");
+            let width = mount.current.clientWidth / 3;
+            let height = width; // mount.current.clientHeight;
             renderer.setSize(width, height, false);
             camera.aspect = width / height;
             camera.updateProjectionMatrix();
         };
 
         const onMouseMove = (event) => {
-            mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-            mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-            absoluteMouse.x = event.clientX;
-            absoluteMouse.y = event.clientY;
-        }
-        
+            var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+            // console.log(event.clientX - renderer.domElement.offsetLeft, scrollTop + event.clientY - renderer.domElement.offsetTop)
+            mouse.x = ( ( event.clientX - renderer.domElement.offsetLeft ) / renderer.domElement.clientWidth ) * 2 - 1;
+            mouse.y = - ((scrollTop + event.clientY - renderer.domElement.offsetTop) / renderer.domElement.clientHeight) * 2 + 1
+        };
+
         const hover = () => {
             raycaster.setFromCamera(mouse, camera);
             // calculate objects intersecting the picking ray
             var intersects = raycaster.intersectObjects(scene.children);
+            // scene.add(new THREE.ArrowHelper( raycaster.ray.direction, raycaster.ray.origin, 100, Math.random() * 0xffffff ));
+            // console.log(raycaster, intersects)
             if (intersects[0] && intersects[0].object.__id) {
-                document.getElementById("info").innerHTML =
-                    events[intersects[0].object?.__id].info;
-                document.getElementById("the_line").setAttribute("x2", absoluteMouse.x);
-                document.getElementById("the_line").setAttribute("y2", absoluteMouse.y);
+                console.log("set", events[intersects[0].object?.__id].data)
+                setEventData(events[intersects[0].object?.__id].data )
             }
-        }
-        
+        };
+
         const renderPlanet = () => {
             THREE.ImageUtils.crossOrigin = "";
             var geometry = new THREE.SphereGeometry(RADIUS, 100, 100);
@@ -102,32 +113,34 @@ const Planet = () => {
                 "https://res.cloudinary.com/hackjunction/image/upload/v1593019499/test/test/junction-website-globe-01-fitted_FIX.png",
             );
             omaterial.transparent = true;
-        
+
             const omesh = new THREE.Mesh(geometry, omaterial);
             scene.add(omesh);
-        }
-        
+        };
+
         const renderDataPoints = () => {
             Object.keys(events).forEach((k) => {
                 const e = events[k];
-                scene.add(renderEvent(...latlonToXZY(e.lat, e.lon), e.__id, e.color));
+                scene.add(
+                    renderEvent(...latlonToXZY(e.lat, e.lon), e.__id, e.color),
+                );
             });
-        }
-        
+        };
+
         const renderEvent = (x, z, y, index, color) => {
             console.log(x, y, z, index, color);
             const geometry = new THREE.BoxGeometry(1, 1, 1);
             const material = new THREE.MeshBasicMaterial({ color: color });
             const cube = new THREE.Mesh(geometry, material);
             cube.position.set(x * RADIUS, y * RADIUS, z * RADIUS);
-        
+
             cube.lookAt(0, 0, 0);
             cube.scale.y = SIZE;
             cube.scale.x = SIZE;
             cube.__id = index;
-        
+
             return cube;
-        }
+        };
 
         const latlonToXZY = (lat, lon) => {
             var cosLat = Math.cos((lat * Math.PI) / 180.0);
@@ -135,17 +148,16 @@ const Planet = () => {
             var cosLon = Math.cos((lon * Math.PI) / 180.0);
             var sinLon = Math.sin((lon * Math.PI) / 180.0);
             return [cosLat * cosLon, cosLat * sinLon, sinLat];
-        }
-        
+        };
+
         const animate = () => {
-            console.log("animating")
             hover();
             renderer.render(scene, camera);
             requestAnimationFrame(animate);
         };
 
         renderPlanet();
-        renderDataPoints();    
+        renderDataPoints();
 
         mount.current.appendChild(renderer.domElement);
         window.addEventListener("resize", handleResize);
@@ -155,12 +167,9 @@ const Planet = () => {
     }, []);
 
     return (
-        <div
-            className="vis"
-            ref={mount}
-        >
-            <EventCard />
-        </div>    
+        <div className={classes.vis} ref={mount}>
+            <EventCard {...eventData} />
+        </div>
     );
 };
 
