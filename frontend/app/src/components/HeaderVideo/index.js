7b55993738637c3a95997c8a05fd7314f912a9ff
Visa Pollari <visapollari@Visas-MBP.home>
2020-07-27 12:21:05 +0300

    Add animation to mobile

--- frontend/app/src/components/HeaderVideo/index.js
+++ frontend/app/src/components/HeaderVideo/index.js
@@ -1,9 +1,9 @@
-import React, { PureComponent } from "react"
-import MediaQuery from "react-responsive"
+import React, { PureComponent } from "react";
+import MediaQuery from "react-responsive";
 
-import "./style.scss"
-import LinkButton from "../LinkButton/"
-import Divider from "../Divider"
+import "./style.scss";
+import LinkButton from "../LinkButton/";
+import Divider from "../Divider";
 
 const Video = ({ onLoad, src }) => {
     return (
@@ -27,37 +27,37 @@ const Video = ({ onLoad, src }) => {
                 <source type="video/mp4" src={src} />
             </MediaQuery>
         </video>
-    )
-}
+    );
+};
 
 class HeaderVideo extends PureComponent {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
             loaded: false,
-        }
+        };
 
-        this.setLoaded = this.setLoaded.bind(this)
+        this.setLoaded = this.setLoaded.bind(this);
     }
 
     setLoaded() {
         this.setState({
             loaded: true,
-        })
+        });
     }
 
     render() {
-        const { loaded } = this.state
-        const src = this.props.src
+        const { loaded } = this.state;
+        const src = this.props.src;
         return (
             <div
                 className={`HeaderVideo ${loaded ? "HeaderVideo-visible" : ""}`}
             >
                 <Video onLoad={this.setLoaded} src={src} />
             </div>
-        )
+        );
     }
 }
 
-export default HeaderVideo
+export default HeaderVideo;
