ab532a6547ebfe42b7f7ac44a19a8729d3e0f4f3
WelehoBRUDER <dimmedd@hotmail.com>
2020-08-11 11:54:27 +0300

    increased strapi limit from 100 => 1000
    as it says

--- frontend/app/src/services/staticcontent.js
+++ frontend/app/src/services/staticcontent.js
@@ -1,7 +1,7 @@
 import axios from './jsonAxios'
 import config from './config'
 
-const URL = config.API_BASE_URL + '/api/staticcontents'
+const URL = config.API_BASE_URL + '/api/staticcontents?_limit=1000'
 
 const StaticContentService = {
 
