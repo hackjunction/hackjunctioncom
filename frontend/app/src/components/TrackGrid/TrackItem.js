eb9b0ebef80d2853b55d05d31cfe44ff38a53c20
1UPNuke <65999898+1UPNuke@users.noreply.github.com>
2020-08-24 12:50:20 +0300

    Fixed flip animation

--- frontend/app/src/components/TrackGrid/TrackItem.js
+++ frontend/app/src/components/TrackGrid/TrackItem.js
@@ -5,21 +5,38 @@ import Markdown from "../Markdown";
 import { requirePropFactory } from "@material-ui/core";
 
 const TrackItem = ({ item }) => {
-    const { title, desc, fulldesc, logos, key } = item;
-    const [open, toggleOpen] = useState(false);
+    //Animation duration
+    const aDur = 0.75
+    const { title, desc, fulldesc, logos, key } = item
+    const [open, toggleOpen] = useState(false)
+
+    const flipStyle = (e, state) => {
+        e.currentTarget.classList.toggle("flip", state)
+        e.currentTarget.doneAnimating = !state
+    }
+    const flipCard = (e) => {
+        if (!e.currentTarget.doneAnimating)
+        {
+            return
+        }
+        let style = window.getComputedStyle(e.currentTarget)
+        let duration = aDur * 1000
+        flipStyle(e, true);
+        setTimeout(()=>toggleOpen(!open), duration/2)
+    }
 
     if (open) {
         return (
-            <div className="TrackItem open" onClick={() => toggleOpen(!open)}>
+            <div className="TrackItem open flip" style={{ animationDuration: aDur + "s" }} onAnimationEnd={(e) => flipStyle(e, false)} onClick={(e) => flipCard(e)}>
                 <div className="TrackItem--text">
                     <span className="TrackItem--title">{title}</span>
                     <p className="TrackItem--description">{fulldesc}</p>
                 </div>
             </div>
-        );
+        )
     }
     return (
-        <div className="TrackItem" onClick={() => toggleOpen(!open)}>
+        <div className="TrackItem closed flip" style={{animationDuration: aDur + "s"}} onAnimationEnd={(e) => flipStyle(e, false)} onClick={(e) => flipCard(e)}>
             <div className="TrackItem--text">
                 <span className="TrackItem--title">{title}</span>
                 <p className="TrackItem--description">{desc}</p>
@@ -30,7 +47,7 @@ const TrackItem = ({ item }) => {
                         <img className="TrackItem--logo" src={logo} />
                     ))}
             </div>
-        </div>
+        </div >
     );
 };
 
