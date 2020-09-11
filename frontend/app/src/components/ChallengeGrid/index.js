5b63792457f134cfc42af4f8a7be48a96f985995
Visa Pollari <visapollari@Visas-MacBook-Pro.local>
2020-09-11 13:56:23 +0300

    challenges button

--- frontend/app/src/components/ChallengeGrid/index.js
+++ frontend/app/src/components/ChallengeGrid/index.js
@@ -1,53 +1,58 @@
-import React, { useState, useEffect } from "react"
-import Button from "../Button"
-import "./style.scss";
+import React, { useState, useEffect } from 'react'
+import Button from '../Button'
+import './style.scss'
 
-import ChallengeItem from "./ChallengeItem";
+import ChallengeItem from './ChallengeItem'
 
 function ChallengeGrid({ items }) {
-    const distincttags = [...new Set(items.map((x) => x.tags.split(', ')).flat())].sort();
+  const distincttags = [
+    ...new Set(items.map((x) => x.tags.split(', ')).flat()),
+  ].sort()
 
-    const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState('')
 
-    const renderButtons = () => {
-        const newselection = (key) => {
-            if (selected == key) {
-                setSelected('');
-            } else {
-                setSelected(key);
-            }
-            return selected;
-        };
-
-        return (
-            distincttags &&
-            distincttags.flat().map((tag) => (
-                <Button
-                    className={`Button-default ${ 
-                        selected == tag ? "selected" : ""
-                    }`}
-                    text={tag}
-                    onClick={() => newselection(tag)}
-
-                />
-            ))
-        );
-    };
+  const renderButtons = () => {
+    const newselection = (key) => {
+      if (selected == key) {
+        setSelected('')
+      } else {
+        setSelected(key)
+      }
+      return selected
+    }
 
     return (
-        <>
-            <div className="FilterButtons">
+      distincttags &&
+      distincttags
+        .flat()
+        .map((tag) => (
+          <Button
+            className={`Button-default ${selected == tag ? 'selected' : ''}`}
+            text={tag}
+            onClick={() => newselection(tag)}
+          />
+        ))
+    )
+  }
+
+  return (
+    <>
+      {/*<div className="FilterButtons">
                 <h3>Click to filter challenges by specifics:</h3>
                 {renderButtons()}
-            </div>
-            <div className="ChallengeGrid">{
-                items.map(item => {
-                    console.log(item);
-                    return item.tags.split(', ').some((a) => {return(selected == a || selected == '')}) ? <ChallengeItem {...{ item }} /> : null
-                })
-            }</div>
-        </>
-    );
+    </div>*/}
+      <div className="ChallengeGrid">
+        {items.map((item) => {
+          console.log(item)
+          return item.tags.split(', ').some((a) => {
+            return selected == a || selected == ''
+          }) ? (
+            <ChallengeItem {...{ item }} />
+          ) : null
+        })}
+      </div>
+    </>
+  )
 }
 
-export default ChallengeGrid;
+export default ChallengeGrid
