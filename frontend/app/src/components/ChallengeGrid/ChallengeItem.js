b572ef895c8e8d45e4808e59c1289a2cfdd00751
Visa Pollari <visa.pollari@gmail.com>
2020-09-30 21:56:27 +0300

    New challenge component

--- frontend/app/src/components/ChallengeGrid/ChallengeItem.js
+++ frontend/app/src/components/ChallengeGrid/ChallengeItem.js
@@ -7,7 +7,20 @@ import { requirePropFactory } from '@material-ui/core'
 const ChallengeItem = ({ item }) => {
   //Animation duration
   const aDur = 0.6
-  const { name, shorttext, long_text, tags, key } = item
+  const {
+    name,
+    company,
+    shorttext,
+    long_text,
+    thechallenge,
+    insights,
+    price,
+    judging,
+    aboutcompany,
+    insightimage,
+    tags,
+    key,
+  } = item
   var { cardbackground, icon } = item
   if (!cardbackground) {
     cardbackground = { url: '' }
@@ -39,11 +52,32 @@ const ChallengeItem = ({ item }) => {
         onClick={(e) => flipCard(e)}
       >
         <div className="ChallengeItem--text">
-          <span className="ChallengeItem--title">{shorttext}</span>
+          <img class="ChallengeIco open" src={icon.url} />
+          <span href="#" class="close"></span>
+          <span className="ChallengeItem--title">{name}</span>
+          <p className="ChallengeItem--description">{shorttext}</p>
           <p className="ChallengeItem--description">{long_text}</p>
-        </div>
-        <div className="ChallengeItem--logowpr">
-          <img class="ChallengeIco" src={icon.url} />
+          <div className="ChallengeItem--subsection">
+            <p className="ChallengeItem--left">The Challenge</p>
+            <p className="ChallengeItem--right">{thechallenge}</p>
+          </div>
+          <div className="ChallengeItem--subsection">
+            <p className="ChallengeItem--left">Insights</p>
+            <p className="ChallengeItem--right">{insights}</p>
+          </div>
+          {insightimage && <img class="InsightImg" src={insightimage.url} />}
+          <div className="ChallengeItem--subsection">
+            <p className="ChallengeItem--left">The Price</p>
+            <p className="ChallengeItem--right">{price}</p>
+          </div>
+          <div className="ChallengeItem--subsection">
+            <p className="ChallengeItem--left">Judging Criteria</p>
+            <p className="ChallengeItem--right">{judging}</p>
+          </div>
+          <div className="ChallengeItem--subsection">
+            <p className="ChallengeItem--left">About the company</p>
+            <p className="ChallengeItem--right">{aboutcompany}</p>
+          </div>
         </div>
       </div>
     )
@@ -58,12 +92,12 @@ const ChallengeItem = ({ item }) => {
       onAnimationEnd={(e) => flipStyle(e, false)}
       onClick={(e) => flipCard(e)}
     >
+      <div className="ChallengeItem--logowpr">
+        <img class="ChallengeIco" src={icon.url} />
+      </div>
       <div className="ChallengeItem--text">
         <span className="ChallengeItem--title">{name}</span>
         <p className="ChallengeItem--description">{shorttext}</p>
-      </div>
-      <div className="ChallengeItem--logowpr">
-        <img class="ChallengeIco" src={icon.url} />
       </div>
     </div>
   )
